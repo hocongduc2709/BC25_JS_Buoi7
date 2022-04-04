bfbbc1e992a9d76cc448ee80eaba4b002923e73e
hocongduc2709 <hocongduc2709@gmail.com>
2022-04-04 11:04:15 +0700

    new file

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+---------------------------- VÒNG LẶP WHILE ---------------------
+*/
+console.log("---------------- VÒNG LẶP WHILE ---------------------");
+
+var n = 0;
+
+while(n < 6)
+{
+    //thực hiện hành động
+    console.log("Hello!");
+
+    //bước nhảy / điều kiện dừng vòng lặp
+    n++;
+}
